f88d25418beae778fa50ca2f12e37798ba76c9bd
familypizza <familypizza2005@gmail.com>
2017-03-17 22:25:51 +0700

    Revert "Revert "first website upload""
    This reverts commit d4eaa455440b871537470932903cd23e92d32d12.

---
+++ js/checkout.js
@@ -0,0 +1,45 @@
+﻿ $(function(){
+
+ 	new WOW().init();
+
+ 	// EDIT CHO NUT TO TOP
+ 	$(window).scroll(function(event) {
+ 		if ($('body').scrollTop()>500) {
+ 			$('.to_top').addClass('len');
+ 		}
+ 		else if ($('body').scrollTop()<=500){
+ 			$('.to_top').removeClass('len');
+ 		}
+ 	});
+
+ 	$('.to_top').click(function(event) {
+ 		$('body').animate({scrollTop: 0}, 500);
+ 	});
+ 	// END
+
+ 	// EDIT CHO NUT BUTTON MENU RESPONSIVE
+ 	$('.topmenu button.navbar-toggler').click(function(event) {
+ 		$(this).toggleClass('vang');
+ 	});
+ 	// END
+
+
+ 	// EDIT CHO TOPMENU
+ 	$(window).scroll(function(event) {
+ 		if ($('body').scrollTop()>=250) {
+ 			$('.topmenu').addClass('hienthi');
+ 		}
+ 		else{
+ 			$('.topmenu').removeClass('hienthi');
+ 		}
+ 		
+ 	});
+ 	// END
+ 	
+
+ 
+
+
+
+})  
+ 
